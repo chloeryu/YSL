171797004f2161459b8fa04fa3dce0dcd26bc131
chloeryu <yeonhee8012@gmail.com>
2023-07-09 19:10:18 +0900

    MongoDB에 데이터 저장하는 법,<form>으로 전송한 데이터 받는 법

--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -1,3 +1,9 @@
-export default function handler(요청, 응답) {
-  console.log(요청.body)
-}
+import { connectDB } from "@/util/database"
+
+export default async function handler(요청, 응답) {
+  if (요청.method == 'POST') {
+    let db = (await connectDB).db('forum')
+    let result = db.collection('post').insertOne(요청.body)
+    응답.redirect(302, '/list')
+  }
+} 
