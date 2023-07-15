3aa4c49a8a8c57a8a9b5bdd9df43daa525359a24
chloeryu <yeonhee8012@gmail.com>
2023-07-15 18:11:06 +0900

    _id: 현재URL에 입력한 id

--- app/edit/[id]/page.js
+++ app/edit/[id]/page.js
@@ -1,11 +1,17 @@
-export default function Edit() {
+import { connectDB } from "@/util/database"
+
+export default async function Edit() {
+
+  const db = (await connectDB).db("forum")
+  let result = await db.collection('post').findOne({ _id: 현재URL에 입력한 id })
+
   return (
     <div className="p-20">
-      <h4>글작성</h4>
+      <h4>수정페이지</h4>
       <form action="/api/post/new" method="POST">
         <input name="title" placeholder="글제목" />
         <input name="content" placeholder="글내용" />
-        <button type="submit">전송</button>
+        <button type="submit">버튼</button>
       </form>
     </div>
   )
--- app/list/page.js
+++ app/list/page.js
@@ -13,7 +13,7 @@ export default async function List() {
           <div className="list-item" key={i}>
             <Link href={'/detail/' + result[i]._id}><h4>{a.title}</h4></Link>
             {/*  <DetailLink /> */} {/* 버튼 */}
-            <Link href={'/edit' + result[i]._id}>👧</Link>
+            <Link href={'/edit/' + result[i]._id}>👧</Link>
             <p>6월 22일</p>
           </div>
         )
